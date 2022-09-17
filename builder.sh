76be6fb2d6ee12c16b9ac18d400899d3e92e30a8
amias-burrows <amias.burrows@gmail.com>
2022-09-17 17:56:57 +0100

    Coded the saving a template structure
    I've got a few issues to iron out still:
    
    - An empty directory, when going through the saving code block, will
      show it having a file called *.* within it.
    
    - The saving to a file bit still doesn't work with the variables that
      have the location saved.
    
    I have still left the debugging characters in the seperator characters
    
    REC_SEP is |  rather than U+001E
    GRP_SEP is {} rather than U+001D

--- builder.sh
+++ builder.sh
@@ -3,43 +3,113 @@
 FILE_LOC="~/.template-builder/files/"
 TEMP_LOC="~/.template-builder/.templates"
 ROOT=$(pwd)
+REC_SEP="|"
+GRP_SEP="{}"
+TXT_SRT="\u0002"
 
-if ( -f $TEMP_LOC )	# Checks if the file that saves the templates in exists.  If not it will create the file and then refer to the help command to give user assistance
-then
+add() {		# Adds file to template directory
+	printf "\nAdd function\n"
+}
 
-	case $1 in
 
-		--save|-s)	# Saves the current directory as a template
+save () {	# Saves the 
 
-			printf "\nSaving the directory as a template structure\n"
+	printf "\nDirectory ID - ${CUR_LOC}\n"	# DEBUGGING
 
-			if ( $2 != "" )		# Tests to see if the template name exists.  If not the user is asked to enter it
-			then
+	for FILE in *		# Runs through every file in the current directory
+	do
 
-				OUTPUT=$2
-			else
+		if [ -d $FILE ]
+			# Checks if it is a Directory or File
+		then
+				# If it's a directory it marks it's parent directory, notes down the details for this directory and enters it to discover all the files within the directory
 
-				printf "\nWhat would you like to name the template structure?"
+			PARENT=$CUR_LOC
+
+				# | File name | Parent Directory ID | Dir\File | Current ID |
+			OUTPUT+="${FILE}${REC_SEP}${CUR_LOC}${REC_SEP}0${REC_SEP}${DIR_COUNT}${GRP_SEP}"
+			printf "\nDirectory - ${FILE}\n"	# DEBUGGING
+
+			CUR_LOC=$DIR_COUNT	# Sets location to the ID of this directory and updates the Directory count for the next directory to have a unique ID
+			((DIR_COUNT++))
+
+			cd ./${FILE}	# Enters the current directory, runs the function within the directory and then leaves after the function has finished
+			save
+			cd ../
+
+			CUR_LOC=$PARENT		# Updates the current locoation back to this Directory so the files don't get given the wrong parent
+		else
+
+			NAME=${FILE%.*}		# Extrapolates File Name
+			printf "\nFile Name - ${NAME}\n"	# DEBUGGING
+
+			EXT=${FILE##*.}		# Extrapolates File Extension
+			printf "\nFile Extension - ${EXT}\n"	# DEBUGGING
+
+				# | File name | Parent Directory ID | Dir\File | File Type |
+			OUTPUT+="${NAME}${REC_SEP}${CUR_LOC}${REC_SEP}1${REC_SEP}${EXT}${GRP_SEP}"
+			printf "\nFile - ${FILE}\n"		# DEBUGGING
+		fi
+	done
+}
+
+case $1 in
+
+	--save|-s)	# Saves the current directory as a template
+		CUR_LOC=0	# Initialises the directory ID for the root directory and indicates that there is one directory in the project
+		DIR_COUNT=1
+
+		printf "\nSaving the directory as a template structure\n"	# Indicates to the user that something is happening
+
+		printf "\nTemplate name: ${2}\n"	# DEBUGGING
+		if [ "${2}" != "" ]		# Tests to see if the template name exists.  If not the user is asked to enter it
+		then
+
+			OUTPUT="${2}${GRP_SEP}"
+		else
+			name_template() {	# Requests name from the user for the template and handles empty name
+				printf "\nWhat would you like to name the template structure? "
 				read OUTPUT
-				printf "\n"
-			fi
 
-			for FILE in *		# Runs through every file in the current directory.  **Will fill more in when the code is built upon**
-			do
-				if ( -d $FILE )
+				if [ -z $OUTPUT ]	# Checks if user has entered anything
 				then
+
+					printf "\nPlease name the template for your convenience later on\n"
+					name_template	# Calls the question again to get a usable response
 				else
+
+					OUTPUT+="${GRP_SEP}"
+					printf "\n"	# Prints a line break to keep the next print on it's own line.  Nothing worse than leaving a script and the input line is on the end of the last bit of text
 				fi
-			done
-			;;
+			}
 
-		--help|-h)
+			name_template	# Calls the naming function above.  It's in a function to act as a 'goto' command for if someone uses it wrong
+		fi
 
-			;;
-	esac
-else
+		save
 
-	touch $TEMP_LOC
-	printf "\nInitialised the template builder"
-	~/.template-builder/builder.sh -h
-fi
+		printf "\n${OUTPUT}\n"		# DEBUGGING
+
+		if [ -f $TEMP_LOC ]	# If the file exists then we add a linebreak to the start of the output.  This ensures that if we have to create a file for this then we won't have an empty line at the start of the file or no line breaks to differenciate the different templates
+		then
+
+			$OUTPUT="\n${OUTPUT}"
+		fi
+
+		echo $OUTPUT >> $TEMP_LOC	# Error prone bit.  Don't know why but it's showing an error of 'no such file or directory' at the location in $TEMP_LOC whilst if I just copy the text in that variable and paste it there it works.  The command >> should create a file if one doesn't exist
+		;;
+
+	--add|-a)	# Adds file to template files
+		if [ -z $2 ]
+		then
+			printf ""	# Temp
+		else
+			printf ""	# Temp
+		fi
+
+		;;
+
+	--help|-h|*)
+		printf "\nHelp Page\n"
+		;;
+esac
