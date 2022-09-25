5987da27a8b024e8fb462a0eb5f23d17f49949e8
Amias-Burrows <amias.burrows@gmail.com>
2022-09-25 20:37:42 +0100

    Cleaning code so the reusable is reusable

--- builder.sh
+++ builder.sh
@@ -10,16 +10,22 @@ RED=$(tput setaf 1)
 BOLD=$(tput bold)
 UNDERLINE=$(tput smul)
 
+	# File locations
+
 FILE_LOC=~/.template-builder/files/
 TEMP_LOC=~/.template-builder/.templates
 ROOT=$(pwd)
+
+	# Special characters
+
 REC_SEP="\u001E"
 GRP_SEP="\u001D"
 TXT_SRT="\u0002"
-#set -x
 
-get_temp_line() {
-	while IFS= read -r LINE		# Runs through the file contents and prints out template names
+set -x	# DEBUGGING
+
+get_temp_line() {		# Finds the correct template from a given name
+	while IFS= read -r LINE
 	do
 
 		if ( $1 == ${LINE%%\\u001D*} )
@@ -31,123 +37,19 @@ get_temp_line() {
 }
 
 
-add() {		# Adds file to template directory
-	printf "\nAdd function\n"
 
-	FILE_COUNT=$(ls ${FILE_LOC} | wc -l)
-	EXT=${2##*.}
+name_try_again() {	# Requests name from the user for the template and handles empty name
+	printf "\nWhat would you like to name the ${1}? "
+	read local RESPONSE
 
-	cp ./${2} ${FILE_LOC}${FILE_COUNT}.${EXT}
-
-	mv ./${2} ./${FILE_COUNT}${2}
-
-	if [ $(echo $?) == 0 ]
-	then
-		printf "\nFile copied into the template directory\n"
-	else
-		printf "\n${RED}${BOLD}Error!${NORMAL} File not copied successfully. Error code 001\n"
-	fi
-}
-
-
-store_file() {	# Asks user if they want this file in the template.  Copies the file over to the template folder if it is wanted
-
-	printf "\nDo you want to store ${Blue}${BOLD}${FILE}${NORMAL} in the template? Y/N "
-	read RESPONSE
-
-	if [ $RESPONSE == "Y" ]	|| [ $RESPONSE == "y" ]
-	then		# Adds file to template folder and gets ID and NAME
-
-		add null $FILE
-		get_id
-
-	elif [ $RESPONSE == "N" ] || [ $RESPONSE == "n" ]
-	then		# Ignores the file and continues with the rest of the program
-
-	
-		printf "\nIgnoring ${BLUE}${BOLD}${FILE}${NORMAL}\n"
-		IGNORE=true
-
-	else		# Handles error from user not entering correct input
-
-		printf "\nPlease respond using either ${YELLOW}${BOLD}Y${NORMAL} or ${GREEN}${BOLD}N${NORMAL}\n"
-		store_file
-
-	fi
-}
-
-
-get_id() {
-	TEMP_ID=${NAME%*}	# Extrapolates ID from File Name
-
-	NAME=${NAME##*}	# Extrapolates File Name from File Name
-}
-
-
-save() {	# Saves the template structure
-
-	for FILE in *		# Runs through every file in the current directory
-	do
-
-		if [ -d $FILE ]
-			# Checks if it is a Directory or File
-		then
-				# If it's a directory it marks it's parent directory, notes down the details for this directory and enters it to discover all the files within the directory
-
-			PARENT=$CUR_LOC
-
-				# | File name | Parent Directory ID | Dir\File | Current ID |
-			OUTPUT+="${FILE}${REC_SEP}${CUR_LOC}${REC_SEP}0${REC_SEP}${DIR_COUNT}${GRP_SEP}"
-
-			CUR_LOC=$DIR_COUNT	# Sets location to the ID of this directory and updates the Directory count for the next directory to have a unique ID
-			((DIR_COUNT++))
-
-			cd ./${FILE}	# Enters the current directory, runs the function within the directory and then leaves after the function has finished
-			save
-			cd ../
-
-			CUR_LOC=$PARENT		# Updates the current locoation back to this Directory so the files don't get given the wrong parent
-		else		# Handles files
-			IGNORE=false
-
-			NAME=${FILE%.*}		# Extrapolates File Name
-
-			EXT=${FILE##*.}		# Extrapolates File Extension
-
-			if [[ "${NAME}" == *""* ]]	# Checks if file name has special character in it
-			then
-
-				get_id
-			else
-
-				store_file
-			fi
-
-			if [ $IGNORE == false ]		# Checks whether the file is being ignored or not
-			then
-
-					# | File name | Parent Directory ID | Dir\File | File Type |
-				OUTPUT+="${NAME}${REC_SEP}${CUR_LOC}${REC_SEP}1${REC_SEP}${TEMP_ID}${GRP_SEP}"
-			fi
-			IGNORE=false
-		fi
-	done
-}
-
-
-name_template() {	# Requests name from the user for the template and handles empty name
-	printf "\nWhat would you like to name the template structure? "
-	read OUTPUT
-
-	if [ -z $OUTPUT ]	# Checks if user has entered anything
+	if [ -z $RESPONSE ]	# Checks if user has entered anything
 	then
 
-		printf "\nPlease name the template for your convenience later on\n"
-		name_template	# Calls the question again to get a usable response
+		printf "\nPlease name the ${1} for your convenience later on\n"
+		name_try_again ${1}	# Calls the question again to get a usable response
 	else
 
-		OUTPUT+="${GRP_SEP}"
-		printf "\n"	# Prints a line break to keep the next print on it's own line.  Nothing worse than leaving a script and the input line is on the end of the last bit of text
+		echo "${RESPONSE}${GRP_SEP}"
 	fi
 }
 
@@ -155,27 +57,104 @@ name_template() {	# Requests name from the user for the template and handles emp
 case $1 in
 
 	--save|-s)	# Saves the current directory as a template
-		CUR_LOC=0	# Initialises the directory ID for the root directory and indicates that there is one directory in the project
-		DIR_COUNT=1
+		local OUTPUT-""
+		local CUR_LOC=0	# Initialises the directory ID for the root directory and indicates that there is one directory in the project
+		local DIR_COUNT=1
 
 		printf "\nSaving the directory as a template structure\n"	# Indicates to the user that something is happening
 
 		if [ "${2}" != "" ]		# Tests to see if the template name has been entered.  If not the user is asked to enter it
 		then
-			CONFLICT=$(get_temp_line $2)
+			local CONFLICT=$(get_temp_line $2)
 
 			if [ $CONFLICT != "" ]
 			then
 				OUTPUT="${2}${GRP_SEP}"
 			else
 				printf "\nThe name ${2} seems to be taken.  Try another one or exit with ${YELLOW}${BOLD}ctrl C${NORMAL}\n"
-				name_template
+				OUTPUT=$(name_try_again "Template Structure")
 			fi
 
 		else
 
-			name_template	# Calls the naming function above.  It's in a function to act as a 'goto' command for if someone uses it wrong
+			name_template	# Calls the naming function above.  It's in a function to act as a 'goto' command for when someone uses it wrong
 		fi
+
+		save() {	# Saves the template structure
+
+			for FILE in *		# Runs through every file in the current directory
+			do
+
+				if [ -d $FILE ]
+					# Checks if it is a Directory or File
+				then
+						# If it's a directory it marks it's parent directory, notes down the details for this directory and enters it to discover all the files within the directory
+
+					local PARENT=$CUR_LOC
+
+						# | File name | Parent Directory ID | Dir\File | Current ID |
+					local OUTPUT+="${FILE}${REC_SEP}${CUR_LOC}${REC_SEP}0${REC_SEP}${DIR_COUNT}${GRP_SEP}"
+
+					local CUR_LOC=$DIR_COUNT	# Sets location to the ID of this directory and updates the Directory count for the next directory to have a unique ID
+					((DIR_COUNT++))
+
+					cd ./${FILE}	# Enters the current directory, runs the function within the directory and then leaves after the function has finished
+					save
+					cd ../
+
+					local CUR_LOC=$PARENT		# Updates the current locoation back to this Directory so the files don't get given the wrong parent
+				else		# Handles files
+					local IGNORE=false
+
+					local NAME=${FILE%.*}		# Extrapolates File Name
+
+					local EXT=${FILE##*.}		# Extrapolates File Extension
+
+					if [[ "${NAME}" == *""* ]]	# Checks if file name has special character in it
+					then
+
+						TEMP_ID=${NAME%*}	# Extrapolates ID from File Name
+
+						NAME=${NAME##*}	# Extrapolates File Name from File Name
+					else
+
+						store_file() {	# Asks user if they want this file in the template.  Copies the file over to the template folder if it is wanted
+
+							printf "\nDo you want to store ${Blue}${BOLD}${FILE}${NORMAL} in the template? Y/N "
+							read local RESPONSE
+
+							if [ $RESPONSE == "Y" ]	|| [ $RESPONSE == "y" ]
+							then		# Adds file to template folder and gets ID and NAME
+
+								add null $FILE
+								get_id
+
+							elif [ $RESPONSE == "N" ] || [ $RESPONSE == "n" ]
+							then		# Ignores the file and continues with the rest of the program
+
+
+								printf "\nIgnoring ${BLUE}${BOLD}${FILE}${NORMAL}\n"
+
+							else		# Handles error from user not entering correct input
+
+								printf "\nPlease respond using either ${YELLOW}${BOLD}Y${NORMAL} or ${GREEN}${BOLD}N${NORMAL}\n"
+								store_file
+
+							fi
+						}
+						store_file
+					fi
+
+					if [ $IGNORE == false ]		# Checks whether the file is being ignored or not
+					then
+
+							# | File name | Parent Directory ID | Dir\File | File Type |
+						OUTPUT+="${NAME}${REC_SEP}${CUR_LOC}${REC_SEP}1${REC_SEP}${TEMP_ID}${GRP_SEP}"
+					fi
+					local IGNORE=false
+				fi
+			done
+		}
 
 		save
 
@@ -192,26 +171,40 @@ case $1 in
 
 		if [ -f $2 ]		# Checks if file exists in location specified
 		then
-			add null $2
+			local FILE_COUNT=$(ls ${FILE_LOC} | wc -l)
+			local EXT=${2##*.}
+		
+			cp ./${2} ${FILE_LOC}${FILE_COUNT}.${EXT}
+		
+			mv ./${2} ./${FILE_COUNT}${2}
+		
+			if [ $(echo $?) == 0 ]
+			then
+				printf "\nFile copied into the template directory\n"
+			else
+				printf "\n${RED}${BOLD}Error!${NORMAL} File not copied successfully. Error code 001\n"
+			fi
 		else
 			printf "\nError. The file at ${BLUE}${BOLD}${2}${NORMAL} was not recognised\n"
 		fi
-
 		;;
 
 	--create|-c)	# Uses a template folder structure that's been previously saved
 
 		if [[ "${2}" != "" ]] && [ -f $TEMP_LOC ]
 		then
-			TEMP=$(get_temp_line $2)
+			local TEMP=$(get_temp_line $2)
+			printf "\n${TEMP}\n"
 		else
-			printf "Error"
+			printf "\n${RED}${BOLD}Error${NORMAL} Please enter the name of the template you wish to use.\n"
 		fi
 
 		;;
 
 	--list|-l)		# Lists the saved template files
-		OUTPUT="\nPreparing to print the names of your saved templates\n"
+		local EXISTS=false
+		local OUTPUT="\nPreparing to print the names of your saved templates\n"
+
 		while IFS= read -r LINE
 		do
 
